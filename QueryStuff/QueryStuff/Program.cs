f077adb9fdb514c4c88e4993b9f1bd09ec9613f1
Ad0pt3d <rafimiah2005@gmail.com>
2024-07-24 14:05:31 -0400

    Lambda and Query demos

---
+++ QueryStuff/QueryStuff/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryStuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // the data source
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            // the query
+            var myQuery = from obj in list
+                          where obj > 2
+                          select obj;
+
+            // the execution
+            foreach (var item in myQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(" Now we show the Method Syntax");
+
+            var myMethodSyntax = list.Where(obj => obj > 2);
+
+            // execute the Method
+            foreach (var item in myMethodSyntax)
+            {
+                Console.WriteLine(item);
+            }
+
+            // a mixed version of LINQ query
+            var mixedSyntax = (from obj in list
+                               select obj).Max();
+
+            Console.WriteLine("Max value = " + mixedSyntax);
+        }
+    }
+}
